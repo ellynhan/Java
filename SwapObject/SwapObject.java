777d8f15d3bf7783a84bc87e7d0dae9a2af6a7f2
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2020-10-06 14:04:27 +0900

    9월 22일 수업

---
+++ SwapObject/SwapObject.java
@@ -0,0 +1,25 @@
+public class swapObject {
+	/*class myNumber {
+		int i;
+	}*/
+	public static void main(String[] args) {
+		MyNumber myNum = new MyNumber();
+		
+		myNum.i =10;
+		
+		System.out.print("mynum i : "+ myNum.i);
+		
+		int[] list = {1,2,3,4,5,6};
+		int[] list2 = reverse(list);
+	}
+
+	private static int[] reverse(int[] list) {
+		int[] result = new int[list.length];
+		
+		return result;
+	}
+}
+
+//class myNumber {
+//	int i;
+//}
