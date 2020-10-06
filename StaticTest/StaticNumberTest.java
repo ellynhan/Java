e022a1c7cee8619e5cf33dc8b1ef4f3080c3b54d
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2020-10-06 15:21:13 +0900

    9월 24일 수업

---
+++ StaticTest/StaticNumberTest.java
@@ -0,0 +1,12 @@
+
+public class StaticNumberTest {
+	public static void main(String[] args) {
+		StaticVariableCount s1 = new StaticVariableCount();
+		s1.DisplayStaticVariable();
+		
+		StaticVariableCount s2 = new StaticVariableCount();
+		s2.DisplayStaticVariable();
+		
+		
+	}
+}
