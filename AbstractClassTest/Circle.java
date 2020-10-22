6a532de8cc9884faa674741e22556581f7266a7f
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2020-10-22 18:28:27 +0900

    10월 22일 수업

---
+++ AbstractClassTest/Circle.java
@@ -0,0 +1,16 @@
+
+public class Circle extends GeometricObject {
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
